effede7d3c2fbd20b8acaaf74886fedeb0c67be8
Masato Lin <masatoli@usc.edu>
2020-11-05 22:26:52 -0800

    Set up methods

--- client/app/src/main/java/com/example/syncify/HostActivity.java
+++ client/app/src/main/java/com/example/syncify/HostActivity.java
@@ -5,10 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HostActivity extends MusicPlayerActivity {
-
+    String currentPlaylistName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
+    }
+
+    void play(){
+        //
+    }
+    void pause(){
+        //
+    }
+    void skip(){
+        //
+    }
+    void closeRoom(){
+        //
     }
 }
