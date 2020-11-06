58d56948aa4e2cae6c8f55de80c33f82bdbed9cd
Masato Lin <masatoli@usc.edu>
2020-11-05 22:59:12 -0800

    added test case for hostactivity

---
+++ client/app/src/test/java/com/example/syncify/HostActivityTest.java
@@ -0,0 +1,18 @@
+package com.example.syncify;
+
+import junit.framework.TestCase;
+
+public class HostActivityTest extends TestCase {
+
+    public void testPlay() {
+    }
+
+    public void testPause() {
+    }
+
+    public void testSkip() {
+    }
+
+    public void testCloseRoom() {
+    }
+}
