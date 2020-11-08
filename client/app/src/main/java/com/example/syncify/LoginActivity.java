439825a76220622ce01077485c64644c80e14d45
Matthew <m.opara25@gmail.com>
2020-11-07 17:07:02 -0800

    Rename activity_main.xml to activity_login.xml

--- client/app/src/main/java/com/example/syncify/LoginActivity.java
+++ client/app/src/main/java/com/example/syncify/LoginActivity.java
@@ -9,7 +9,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         //String clientId = getResources().getString(R.string.clientSecret);
     }
 
