cdb0bb7501b39bf8810779eea75e05c7e4b4a7ea
Carolyn Hua <carolynh@usc.edu>
2020-11-08 17:16:19 -0800

    Testing again

--- client/app/src/main/java/com/example/syncify/SearchUserActivity.java
+++ client/app/src/main/java/com/example/syncify/SearchUserActivity.java
@@ -1,16 +1,48 @@
 package com.example.syncify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import java.lang.String;
+import com.google.firebase.database.*;
 
 public class SearchUserActivity extends AppCompatActivity {
     String searchInput;
+    String[] searchResults;
+
+    DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mref = ref.child("searchInput");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_user);
+    }
+
+    public String[] getResults(String searchID) // want to show users that are hosting a room at the current moment
+    {
+        searchInput = searchID;
+        mref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot_each : snapshot.getChildren()){
+                    String name = snapshot_each.getValue(String.class);
+                    searchResults.add(name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        // send http request to server with searchInput
+        // get json object of list of search ID
+        // parse json object to get string of searchIDs
+        // if syntax is wrong, return error
+        // if none are displayed, then display empty string[]
+
+        return searchResults;
     }
 }
