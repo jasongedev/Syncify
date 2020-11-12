c546ab9428fe26e194209d98bd267f12129398ff
Crystal Qin <clqin@usc.edu>
2020-11-11 19:35:55 -0800

    worked on search

--- server/src/Search.java
+++ server/src/Search.java
@@ -21,6 +21,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 /**
@@ -55,6 +56,43 @@ public class Search extends HttpServlet {
 
 		DatabaseReference ref = database.getReference("users");
 		
+		
+		ref.addChildEventListener(new ChildEventListener() {
+
+			@Override
+			public void onChildAdded(DataSnapshot snapshot, String previousChildName) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onChildChanged(DataSnapshot snapshot, String previousChildName) {
+				UserObject u = snapshot.getValue(UserObject.class);
+				if(u.getIsSearching()) {
+					System.out.println(u.getIsSearching());
+				}
+				
+			}
+
+			@Override
+			public void onChildRemoved(DataSnapshot snapshot) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onChildMoved(DataSnapshot snapshot, String previousChildName) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onCancelled(DatabaseError error) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 
 	/**
--- server/src/UserObject.java
+++ server/src/UserObject.java
@@ -3,11 +3,51 @@ public class UserObject {
 	public String name;
 	public Double timestamp;
 	public Boolean isPlaying;
+	public Boolean isSearching;
 	
-	public UserObject(String name, double timestamp, boolean isPlaying) {
+	public UserObject() {
+	
+	}
+	
+	public UserObject(String name, double timestamp, boolean isPlaying, boolean isSearching) {
 		// TODO Auto-generated constructor stub
 		this.name = name;
 		this.timestamp = timestamp;
 		this.isPlaying = isPlaying;
+		this.isSearching = isSearching;
+		
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Double timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Boolean getIsPlaying() {
+		return isPlaying;
+	}
+
+	public void setIsPlaying(Boolean isPlaying) {
+		this.isPlaying = isPlaying;
+	}
+
+	public Boolean getIsSearching() {
+		return isSearching;
+	}
+
+	public void setIsSearching(Boolean isSearching) {
+		this.isSearching = isSearching;
+	}
+	
 }
