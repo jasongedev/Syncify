6c04c0c423af2637d7d8f035bb9fcc92dfb159d3
Matthew <m.opara25@gmail.com>
2020-11-22 04:28:05 -0800

    Add app splash screen

--- client/app/src/main/java/com/example/syncify/LoginActivity.java
+++ client/app/src/main/java/com/example/syncify/LoginActivity.java
@@ -32,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SyncifyTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
