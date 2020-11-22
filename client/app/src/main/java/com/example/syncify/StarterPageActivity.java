a49d91a2ba2fcf24e463d84c0a126e4bd3d862a8
Carolyn Hua <carolynh@usc.edu>
2020-11-21 16:30:08 -0800

    starter page

--- client/app/src/main/java/com/example/syncify/StarterPageActivity.java
+++ client/app/src/main/java/com/example/syncify/StarterPageActivity.java
@@ -12,6 +12,20 @@ public class StarterPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starter_page);
+
+        final Button host_button = findViewById(R.id.Host_Room);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               HostRoom(v);
+            }
+        });
+
+        final Button join_button = findViewById(R.id.Join_Room);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                JoinRoom(v);
+            }
+        });
     }
 
     public void HostRoom(View view) {
